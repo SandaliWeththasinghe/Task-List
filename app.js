8226286bef02d11a45668c8838ae13e941ad5be4
Sandali Weththasinghe <37362261+SandaliWeththasinghe@users.noreply.github.com>
2020-03-30 14:04:19 +0530

    Filter Tasks modified

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ function clearTasks() {
 function filterTasks(e) {
     const text = e.target.value.toLowerCase();
 
-    document.querySelectorAll('collection-item').forEach(function(task){
+    document.querySelectorAll('.collection-item').forEach(function(task){
         const item = task.firstChild.textContent;
         if(item.toLowerCase().indexOf(text) != -1){
             task.style.display = 'block';
